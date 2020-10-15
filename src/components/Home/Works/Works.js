72ea345f440200190b0accf79c7781c75c130596
prodip2416 <prodip.sarker.cse@gmail.com>
2020-10-15 19:55:25 +0600

    last changes

--- src/components/Home/Works/Works.js
+++ src/components/Home/Works/Works.js
@@ -11,27 +11,33 @@ const Works = () => {
         slidesToScroll: 3,
     };
     return (
-        <div style={{marginTop:'70px'}} className="text-center mb-5 works-bg">
-            <div className="text-center"><br/>
-                <h3 className="text-center"><span className="text-white"><br/> Here are some of</span> <span style={{ color: '#7AB259' }}>our works</span> </h3>
+        <div style={{ marginTop: '70px' }} className="text-center mb-5 works-bg">
+            <div className="text-center"><br />
+                <h3 className="text-center"><span className="text-white"><br /> Here are some of</span> <span style={{ color: '#7AB259' }}>our works</span> </h3>
             </div>
-            <Slider {...settings}>
-                <div>
-                    <img src="https://i.ibb.co/L5g5X98/carousel-1.png" className="img-fluid p-3 mt-3" alt="..." />
+            <div className="row">
+                <div className="col-1 col-sm-1 col-md-1 col-lg-1"></div>
+                <div className="col-11 col-sm-11 col-md-11 col-lg-11">
+                    <Slider {...settings}>
+                        <div>
+                            <img src="https://i.ibb.co/L5g5X98/carousel-1.png" className="img-fluid p-3 mt-3" alt="..." />
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/VqzZYYZ/carousel-2.png" className="img-fluid p-3 mt-3" alt="..." />
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/yFqjsWf/carousel-5.png" className="img-fluid p-3 mt-3" alt="..." />
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/2hpWM9y/carousel-4.png" className="img-fluid p-3 mt-3" alt="..." />
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/VqzZYYZ/carousel-2.png" className="img-fluid p-3 mt-3" alt="..." />
+                        </div>
+                    </Slider>
                 </div>
-                <div>
-                    <img src="https://i.ibb.co/VqzZYYZ/carousel-2.png" className="img-fluid p-3 mt-3" alt="..." />
-                </div>
-                <div>
-                    <img src="https://i.ibb.co/yFqjsWf/carousel-5.png" className="img-fluid p-3 mt-3" alt="..." />
-                </div>
-                <div>
-                    <img src="https://i.ibb.co/2hpWM9y/carousel-4.png" className="img-fluid p-3 mt-3" alt="..." />
-                </div>
-                <div>
-                    <img src="https://i.ibb.co/VqzZYYZ/carousel-2.png" className="img-fluid p-3 mt-3" alt="..." />
-                </div>
-            </Slider>
+            </div>
+
             <div style={{ height: '80px' }}></div>
         </div>
     );
